64bd7908416acb685d4265315f5654c2f828c69f
Vinh Tran <vinhquoc2103@gmail.com>
2021-08-04 18:11:29 +0700

    Update Covid.sql

--- Covid 19 - Data Visualization using Tableau/Covid.sql
+++ Covid 19 - Data Visualization using Tableau/Covid.sql
@@ -1,47 +1,51 @@
 # Tableau Profile: https://public.tableau.com/app/profile/vinhtran
 # Create table and import data from csv file
-create table covid
+CREATE TABLE covid 
 (
-	iso_code varchar(255),
-    continent varchar(255),
-    location varchar(255),
-    date date,
-    total_cases int,
-    new_cases int,
-    total_deaths int,
-    new_deaths int,
-    population int
+iso_code VARCHAR(255),
+    continent VARCHAR(255),
+    location VARCHAR(255),
+    date DATE,
+    total_cases INT,
+    new_cases INT,
+    total_deaths INT,
+    new_deaths INT,
+    population INT
 );
 
 
 
 # Global numbers of COVID19
-select
-sum(new_Cases) Total_cases,
-sum(new_deaths) Total_deaths,
-round(sum(new_deaths)*100/sum(new_Cases),1) Death_rate
-from covid;
+SELECT 
+    SUM(new_Cases) Total_cases,
+    SUM(new_deaths) Total_deaths,
+    ROUND(SUM(new_deaths) * 100 / SUM(new_Cases),1) Death_rate
+FROM
+    covid;
 
 
 # Total cases, Total deaths, Death percentage in each continent
-select 
-continent,
-sum(new_Cases) Total_cases,
-sum(new_deaths) Total_deaths,
-round(sum(new_deaths)*100/sum(new_Cases),1) Death_rate
-from covid
-where continent not like ''
-group by continent;
+SELECT 
+    continent,
+    SUM(new_Cases) Total_cases,
+    SUM(new_deaths) Total_deaths,
+    ROUND(SUM(new_deaths) * 100 / SUM(new_Cases),1) Death_rate
+FROM
+    covid
+WHERE
+    continent NOT LIKE ''
+GROUP BY continent;
 
 
 # Percent population infected per country
-select
-Location Country,
-sum(new_cases) Total_cases,
-max(population) Population,
-(sum(new_cases)*100)/max(population) as Percent
-from covid
-group by location;
+SELECT 
+    Location Country,
+    SUM(new_cases) Total_cases,
+    MAX(population) Population,
+    (SUM(new_cases) * 100) / MAX(population) AS Percent
+FROM
+    covid
+GROUP BY location;
 
 
 # Deaths count per continent each month
